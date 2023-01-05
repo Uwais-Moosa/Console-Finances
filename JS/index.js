2fbc295a6e6682693307ec5e643914b76daafa2b
Uwais Moosa <uwais_m@hotmail.co.uk>
2023-01-05 19:45:25 +0000

    correct calculations

--- JS/index.js
+++ JS/index.js
@@ -93,27 +93,26 @@ let months = [];
 
 // Total sum
 let total = 0;
-    for (let i = 0; i < totalMonths; i++){
-        total += finances [i][1];
-    };
+for (let i = 0; i < totalMonths; i++){
+    total += finances [i][1];
+};
 
 // Total changes from one month to the other + average calculation
 let changes = [];
-    for (let j = 1; j < finances.length; j++) {
-      let difference = (finances[j][1] - finances[j-1][1]);
-      changes.push(difference)
-      months.push([finances [j][0], difference]);
-    }
+for (let j = 1; j < finances.length; j++) {
+    let difference = (finances[j][1] - finances[j-1][1]);
+    changes.push(difference)
+};
 
 let sum = 0;
-changes.forEach((num) => { sum += num });
+for (let i = 0; i < changes.length; i++) {
+    sum += changes[i];
+};
 let averageChange = sum / changes.length;
 
 // Greatest Increase and Decrease
-let greatestIncrease = changes => Math.max[changes.length-1];
-let greatestMonth = months[greatestIncrease] ;
-let greatestDecrease = changes[0];
-let lowestMonth = months[greatestDecrease];
+let greatestIncrease = Math.max(...changes);
+let greatestDecrease = Math.min(...changes);
 
 //console.log
 
@@ -122,5 +121,5 @@ console.log(`Financial Analysis
     Total Months: ${totalMonths}
     Total: $ ${total}
     Average  Change: $ ${averageChange}
-    Greatest Increase in Profits: ${greatestMonth} ${greatestIncrease}
-    Greatest Decrease in Profits: ${lowestMonth} ${greatestDecrease}`);
+    Greatest Increase in Profits: $ ${greatestIncrease}
+    Greatest Decrease in Profits: $ ${greatestDecrease}`);
